c6369673e945c233278fc807f4f533a02c9a75f0
project2479 <jaywrsnp6@gmail.com>
2024-08-05 18:22:09 +0800

    registration

--- src/partials/registration/Register.jsx
+++ src/partials/registration/Register.jsx
@@ -3,8 +3,8 @@ import Warnings from "./Warnings";
 function Register() {
 	return (
 		<>
-			<div className="flex flex-col justify-center items-center font-[sans-serif] lg:h-screen p-0 -mt-4">
-				<div className="flex flex-row flex-wrap sm:flex-nowrap items-center bg-white dark:bg-gray-800 w-full shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md overflow-hidden">
+			<div className="flex flex-col justify-center items-center font-[sans-serif] p-0 -mt-4">
+				<div className="flex flex-row flex-wrap sm:flex-nowrap items-center bg-white dark:bg-gray-800 w-full shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md">
 					<form className="sm:p-8 p-4 w-full">
 						<div className="mb-12">
 							<h3 className="text-purple-500 text-3xl font-extrabold max-md:text-center">Register</h3>
@@ -155,6 +155,67 @@ function Register() {
 									placeholder=""
 								/>
 							</div>
+						</div>
+
+						<div className="grid lg:grid-cols-4 gap-6 mt-8">
+							<div>
+								<label className="text-gray-800 dark:text-white text-sm mb-2 block">Engine Make/Serial No.:</label>
+								<input
+									name="name"
+									type="text"
+									className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-purple-500"
+									placeholder=""
+								/>
+							</div>
+							<div>
+								<label className="text-gray-800 dark:text-white text-sm mb-2 block">Cycle:</label>
+								<input
+									name="lname"
+									type="text"
+									className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-purple-500"
+									placeholder=""
+								/>
+							</div>
+							<div>
+								<label className="text-gray-800 dark:text-white text-sm mb-2 block">Number of Cylinder:</label>
+								<input
+									name="lname"
+									type="text"
+									className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-purple-500"
+									placeholder=""
+								/>
+							</div>
+							<div>
+								<label className="text-gray-800 dark:text-white text-sm mb-2 block">Number of Engine:</label>
+								<input
+									name="lname"
+									type="text"
+									className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-purple-500"
+									placeholder=""
+								/>
+							</div>
+						</div>
+
+						<div className="grid lg:grid-cols-2 gap-6 mt-8">
+							<div>
+								<label className="text-gray-800 dark:text-white text-sm mb-2 block">Former Owner:</label>
+								<input
+									name="name"
+									type="text"
+									className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-purple-500"
+									placeholder=""
+								/>
+							</div>
+							<div>
+								<label className="text-gray-800 dark:text-white text-sm mb-2 block">Address:</label>
+								<input
+									name="lname"
+									type="text"
+									className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-purple-500"
+									placeholder=""
+								/>
+							</div>
+							
 						</div>
 
 						<div className="mt-6">
